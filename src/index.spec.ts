a71d5999854bfd1cd82eafaf0d7b322b5c0e667c
Tachibana Shin <45375496+tachibana-shin@users.noreply.github.com>
2022-09-03 08:26:11 +0000

    done

---
+++ src/index.spec.ts
@@ -0,0 +1,145 @@
+import mitt from "mitt"
+import { beforeEach, describe, expect, test } from "vitest"
+
+import type { Receiver, Sender } from "."
+import { receive, send } from "."
+
+const events1 = mitt<{
+  message: any
+}>()
+const events2 = mitt<{
+  message: any
+}>()
+const receiver1: Receiver = {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  postMessage: (data: any) => {
+    events1.emit("message", { data })
+  }
+}
+const sender1: Sender = {
+  addEventListener(name: "message", handler: (...args: any[]) => void) {
+    events2.on("message", handler)
+  },
+  removeEventListener(name: "message", handler: (...args: any[]) => void) {
+    events2.off("message", handler)
+  }
+}
+const receiver2: Receiver = {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  postMessage: (data: any) => {
+    events2.emit("message", { data })
+  }
+}
+const sender2: Sender = {
+  addEventListener(name: "message", handler: (...args: any[]) => void) {
+    events1.on("message", handler)
+  },
+  removeEventListener(name: "message", handler: (...args: any[]) => void) {
+    events1.off("message", handler)
+  }
+}
+
+describe("normal", () => {
+  test("normal", async () => {
+    events1.all.clear()
+    events2.all.clear()
+
+    const controller = receive(
+      {
+        receiver: receiver1,
+        sender: sender1
+      },
+      {
+        sum(a: number, b: number) {
+          return a + b
+        }
+      }
+    )
+    expect<typeof controller>(
+      await send(
+        {
+          receiver: receiver2,
+          sender: sender2
+        },
+        "sum",
+        [1, 2]
+      )
+    ).toEqual(3)
+  })
+  test("error", async () => {
+    events1.all.clear()
+    events2.all.clear()
+
+    const controller = receive(
+      {
+        receiver: receiver1,
+        sender: sender1
+      },
+      {
+        sum(a: number, b: number) {
+          // eslint-disable-next-line functional/no-throw-statement
+          throw new Error("error")
+        }
+      }
+    )
+    await expect(
+      send<typeof controller>(
+        {
+          receiver: receiver2,
+          sender: sender2
+        },
+        "sum",
+        [1, 2]
+      )
+    ).rejects.toThrow("error")
+  })
+  test("not found", async () => {
+    receive(
+      {
+        receiver: receiver1,
+        sender: sender1
+      },
+      {
+        sum(a: number, b: number) {
+          return a + b
+        }
+      }
+    )
+    await expect(
+      send(
+        {
+          receiver: receiver2,
+          sender: sender2
+        },
+        "sum2",
+        [1, 2]
+      )
+    ).rejects.toEqual("NOT_FOUND_ERR")
+  })
+  test("promise", async () => {
+    events1.all.clear()
+    events2.all.clear()
+
+    const controller = receive(
+      {
+        receiver: receiver1,
+        sender: sender1
+      },
+      {
+        async sum(a: number, b: number) {
+          return a + b
+        }
+      }
+    )
+    expect<typeof controller>(
+      await send(
+        {
+          receiver: receiver2,
+          sender: sender2
+        },
+        "sum",
+        [1, 2]
+      )
+    ).toEqual(3)
+  })
+})
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,189 @@
-export * from "./sum"
+/* eslint-disable @typescript-eslint/no-explicit-any */
+interface Sender {
+  addEventListener: (
+    name: "message",
+    handler: (event: MessageEvent<unknown>) => void
+  ) => void
+  removeEventListener: (
+    name: "message",
+    handler: (event: MessageEvent<unknown>) => void
+  ) => void
+}
+interface Receiver {
+  postMessage: (
+    data: any,
+    // eslint-disable-next-line no-undef
+    targetOrigin?: string | WindowPostMessageOptions
+  ) => void
+}
+interface IRequest {
+  id: string
+  name: string
+  // eslint-disable-next-line functional/functional-parameters
+  arguments: unknown[]
+}
+interface IResponseResult {
+  id: string
+  name: string
+  result: unknown
+}
+interface IResponseError {
+  id: string
+  name: string
+  error: string
+}
+
+function isRequest(data: any): data is IRequest {
+  return data && "id" in data && "arguments" in data
+}
+function isResponseResult(data: any): data is IResponseResult {
+  return data && "id" in data && "result" in data
+}
+function isResponseError(data: any): data is IResponseError {
+  return data && "id" in data && "error" in data
+}
+
+// is @server
+function receive<Controllers extends Record<string, (...args: any[]) => any>>(
+  options: {
+    sender: Sender
+    receiver: Receiver
+  },
+  controllers: Controllers,
+
+  targetOrigin?: string
+): Controllers
+// eslint-disable-next-line no-redeclare
+function receive<Controllers extends Record<string, (...args: any[]) => any>>(
+  options: {
+    sender: Sender
+    receiver: Receiver
+  },
+  controllers: Controllers,
+
+  // eslint-disable-next-line no-undef
+  optionsTargetOrigin?: WindowPostMessageOptions
+): Controllers
+// eslint-disable-next-line no-redeclare
+function receive<Controllers extends Record<string, (...args: any[]) => any>>(
+  options: {
+    sender: Sender
+    receiver: Receiver
+  },
+  controllers: Controllers,
+
+  // eslint-disable-next-line no-undef
+  targetOrigin?: string | WindowPostMessageOptions
+): Controllers {
+  options.sender.addEventListener("message", async ({ data }) => {
+    if (isRequest(data)) {
+      // && data.name === names
+
+      if (data.name in controllers) {
+        const controller = controllers[data.name]
+        try {
+          const responseResult: IResponseResult = {
+            id: data.id,
+            name: data.name,
+            result: await controller(...data.arguments)
+          }
+          options.receiver.postMessage(responseResult, targetOrigin)
+        } catch (err) {
+          const responseError: IResponseError = {
+            id: data.id,
+            name: data.name,
+            error: err + ""
+          }
+
+          options.receiver.postMessage(responseError, targetOrigin)
+        }
+      } else {
+        const responseError: IResponseError = {
+          id: data.id,
+          name: data.name,
+          error: "NOT_FOUND_ERR"
+        }
+
+        options.receiver.postMessage(responseError, targetOrigin)
+      }
+    }
+  })
+
+  return controllers
+}
+// is @client
+function send<
+  Controllers extends Record<string, (...args: any[]) => any>,
+  Name extends keyof Controllers = keyof Controllers
+>(
+  options: {
+    receiver: Receiver
+    sender: Sender
+  },
+  name: Name,
+  args: Controllers[Name] extends (...args: infer A) => any ? A : unknown[],
+
+  targetOrigin?: string
+): Promise<Awaited<ReturnType<Controllers[Name]>>>
+// eslint-disable-next-line no-redeclare
+function send<
+  Controllers extends Record<string, (...args: any[]) => any>,
+  Name extends keyof Controllers = keyof Controllers
+>(
+  options: {
+    receiver: Receiver
+    sender: Sender
+  },
+  name: Name,
+  args: Controllers[Name] extends (...args: infer A) => any ? A : unknown[],
+
+  // eslint-disable-next-line no-undef
+  postMessageOptions?: WindowPostMessageOptions
+): Promise<Awaited<ReturnType<Controllers[Name]>>>
+// eslint-disable-next-line no-redeclare
+function send<
+  Controllers extends Record<string, (...args: any[]) => any>,
+  Name extends keyof Controllers = keyof Controllers
+>(
+  options: {
+    receiver: Receiver
+    sender: Sender
+  },
+  name: Name,
+  args: Controllers[Name] extends (...args: infer A) => any ? A : unknown[],
+
+  // eslint-disable-next-line no-undef
+  targetOrigin?: string | WindowPostMessageOptions
+): Promise<Awaited<ReturnType<Controllers[Name]>>> {
+  const id = Math.random().toString(34).slice(2)
+
+  return new Promise<Awaited<ReturnType<Controllers[Name]>>>(
+    (resolve, reject) => {
+      const request: IRequest = {
+        id,
+        name: name as string,
+        arguments: args
+      }
+      options.sender.addEventListener("message", handler)
+      function handler({ data }: MessageEvent<unknown>) {
+        if (isResponseResult(data) && data.id === id && data.name === name) {
+          // this result
+          resolve(data.result as ReturnType<Controllers[Name]>)
+
+          options.sender.removeEventListener("message", handler)
+          return
+        }
+        if (isResponseError(data) && data.id === id && data.name === name) {
+          reject(data.error)
+
+          options.sender.removeEventListener("message", handler)
+        }
+      }
+
+      options.receiver.postMessage(request, targetOrigin)
+    }
+  )
+}
+
+export { receive, send }
+export type { Sender, Receiver, IRequest, IResponseResult, IResponseError }
